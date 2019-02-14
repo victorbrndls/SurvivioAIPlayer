f906f4b2d7f3ab0b583757b2457e2e1e23a84658
Harystolho <haristolhogamer@gmail.com>
2019-02-14 07:49:44 -0200

    Change server in game

--- battleio/__init__.py
+++ battleio/__init__.py
@@ -1,6 +1,8 @@
 from battleio import webBrowser
-from battleio.webBrowser import GameWebBrowser
+from battleio.webBrowser import GameWebBrowser, GameMode
 
 if __name__ == '__main__':
     webBrowser = GameWebBrowser()
     webBrowser.loadGame()
+    webBrowser.changeServer('sa')
+    webBrowser.startGame(GameMode.PLAY_DUO)
--- battleio/webBrowser.py
+++ battleio/webBrowser.py
@@ -1,8 +1,16 @@
 from selenium import webdriver
 
 from battleio import settings
+from enum import Enum
 
 GAME_URL = "https://surviv.io/"
+
+
+class GameMode(Enum):
+    PLAY_SOLO = 'btn-start-solo'
+    PLAY_DUO = 'btn-start-duo'
+    PLAY_SQUAD = 'btn-start-squad'
+
 
 class GameWebBrowser():
     '''
@@ -18,7 +26,31 @@ class GameWebBrowser():
         if settings.driver is None:
             self._createDriver()
         
+        print("Loading the game")
         settings.driver.get(GAME_URL)        
         
     def _createDriver(self):
+        print("Creating the WebDriver")
         settings.driver = webdriver.Firefox(executable_path="D:\\Workspace\\Apps\\DataCrawler\\geckodriver.exe")
+
+    def changeServer(self, server):
+        '''
+        Server:
+            'sa': South America
+            'na': North America
+        '''
+        print("Changing server to {0}".format(server))
+        settings.driver.execute_script("document.querySelector(\"#server-select-main\").value = '{0}'".format(server))
+
+    # Methods that interact with the game
+    def startGame(self, mode):
+        '''
+        Starts the game in the chosen mode(GameMode)
+        '''
+        print("Starting the game [{0}]".format(mode))
+        settings.driver.find_element_by_css_selector("#" + mode.value).click()
+
+    
+    
+    
+    
